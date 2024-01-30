616e96771470a8df6a51cf6b24aed11425db3113
brendanjlewis <brendan.j.f.lewis@gmail.com>
2024-01-30 01:45:31 -0500

    added local storage

--- script.js
+++ script.js
@@ -80,7 +80,13 @@ class App {
     #workouts = [];
 
     constructor() {
+        // Get user's position
         this._getPosition();
+
+        // Get data from local storage
+        this._getLocalStorage();
+
+        // Attach event handlers
         form.addEventListener('submit', this._newWorkout.bind(this));
         inputType.addEventListener('change', this._toggleElevationField);
         containerWorkouts.addEventListener('click', this._moveToPopUp.bind(this));
@@ -111,6 +117,10 @@ class App {
 
         // Handling clicks on map
         this.#map.on('click', this._showForm.bind(this));
+
+        this.#workouts.forEach(work => {
+            this._renderWorkoutMarker(work);
+        })
     }
 
     _showForm(mapE) {
@@ -178,7 +188,8 @@ class App {
         // Hide form and clear input fields
         this._hideForm();
 
-        // Display marker
+        // Set local storage to all workouts
+        this._setLocalStorage();
     }
 
     _renderWorkoutMarker(workout) {
@@ -252,7 +263,7 @@ class App {
         const workoutEl = e.target.closest('.workout');
         console.log(workoutEl);
 
-        if(!workoutEl) return;
+        if (!workoutEl) return;
 
         const workout = this.#workouts.find(work => work.id === workoutEl.dataset.id);
         console.log(workout);
@@ -267,7 +278,26 @@ class App {
         // using the public interface
         workout.click();
     }
+
+    _setLocalStorage() {
+        localStorage.setItem('workouts', JSON.stringify(this.#workouts));
+    }
+
+    _getLocalStorage() {
+        const data = JSON.parse(localStorage.getItem('workouts'));
+
+        if (!data) return;
+
+        this.#workouts = data;
+
+        this.#workouts.forEach(work => {
+            this._renderWorkout(work);
+        })
+    }
+    reset() {
+        localStorage.removeItem('workouts');
+        location.reload();
+    }
 }
 
 const app = new App();
-
